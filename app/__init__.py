85a922058ed2b72f999504a7cdd714ed09f7cd0e
Raivydas Šimėnas <raivydas.simenas@gmail.com>
2022-11-24 14:17:41 +0200

    Refactor db to PostgreSQL. Add migrate functionality

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+from flask_migrate import Migrate
 from app import config
 
 app = Flask(__name__)
@@ -15,8 +16,10 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-with app.app_context():
-    db.create_all()
-    db.session.commit()
+# with app.app_context():
+#     db.create_all()
+#     db.session.commit()
 
 from app import views, models
+
+migrate = Migrate(app, db)
