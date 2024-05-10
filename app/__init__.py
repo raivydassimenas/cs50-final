04b368ca4181fcbfb6f1bc586d9d1aae4c4c9fd5
Raivydas Šimėnas <raivydas.simenas@proton.me>
2024-05-10 14:09:24 +0300

    Add NBA API key

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,30 @@
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
+
 from app import config
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = config.SQLALCHEMY_DATABASE_URI
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, config.SQLALCHEMY_DATABASE_URI)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["SECRET_KEY"] = config.SECRET_KEY
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 
+from app.models import User, Game, Prediction, Access
+
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-# with app.app_context():
-#     db.create_all()
-#     db.session.commit()
+with app.app_context():
+    db.create_all()
+    db.session.commit()
 
 from app import views, models
 
